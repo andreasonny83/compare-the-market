8a02ed1c74dc2f6383a63b1b6d558f3557e1f3eb
Andrea Sonny <andreasonny83@gmail.com>
2016-06-23 21:38:38 +0100

    update

--- test1.js
+++ test1.js
@@ -1,58 +1,91 @@
 var fs = require('fs');
 var remaining = '';
-var totWords = {};
+var uniqueWords = {};
 
+/**
+ * Count the unique words contained in the file stream
+ * and store them in the uniqueWords Object
+ *
+ * @param  {Object} line  The chunk file coming from the read stream
+ *
+ * @return {[type]}      [description]
+ */
 function countWords(line) {
-  var words = line.split(' ');
+  var words;
   var word;
+  var i;
 
-  for (var i = 0; i < words.length; i++) {
-    word = words[i].replace(/^[\'\.\,\;\:\"\!\_\s\d]+|[\'\.\,\;\:\"\!\_\?\s]+$/g, '');
-    // word = word.replace(/('s)$/, '');
-    // word = words[i];
+  words = line.match(/(?!\d)(\w+\b)(?!.*\1)/g, '');
 
-    // exclude empty lines
-    if (!words[i].length) {
-      continue;
-    }
+  for (i = 0; words ? i < words.length : 0; i++) {
+    word = words[i].toLowerCase();
 
-    if (totWords[word]) {
-      totWords[word].count += 1;
-    } else {
-      totWords[word] = {count: 1};
-    }
-
-    // console.log(word);
+    uniqueWords[word] = uniqueWords[word] ?
+      uniqueWords[word] += 1 : 1;
   }
 }
 
-// var input = fs.createReadStream('lines.txt');
-var readStream = fs.createReadStream(
-  'test_files/Railway-Children-by-E-Nesbit.txt',
-   {
-     encoding: 'utf8'
-    //  bufferSize: 64 * 1024
-   }
- );
+/**
+ * Sort the result and render the list on the screen
+ *
+ * @param  {Object} results   The unique words object
+ */
+function printResult(results) {
+  var keysSorted = Object.keys(results)
+        .sort(function(a, b) {
+          return results[a] - results[b];
+        });
+
+  return keysSorted.forEach(function(val) {
+    return val, results[val];
+  });
+}
+
+/**
+ * Read each text file inside the target dyrectory
+ *
+ * @param  {String} dirname   The name of the subfolder
+ */
+function readDir(dirname) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      console.log('error:', err);
+      return;
+    }
+
+    var readStream;
+
+    filenames.forEach(function(filename) {
+      readStream = fs.createReadStream(
+        [dirname, filename].join('/'),
+        {encoding: 'utf8', bufferSize: 1024}
+      );
+
+      readStream.on('data', function(chunk) {
+        var index = remaining.indexOf('\n');
+        var line;
+
+        remaining += chunk;
+
+        while (index > -1) {
+          line = remaining.substring(0, index);
+          remaining = remaining.substring(index + 1);
+          countWords(line);
+          index = remaining.indexOf('\n');
+        }
+      });
+
+      readStream.on('end', function() {
+        if (remaining.length > 0) {
+          countWords(remaining);
+        }
+
+        return printResult(uniqueWords);
+      });
+    });
+  });
+}
 
 
-// readLines(input, func);
 
-readStream.on('data', function(chunk) {
-  remaining += chunk;
-  var index = remaining.indexOf('\n');
-  while (index > -1) {
-    var line = remaining.substring(0, index);
-    remaining = remaining.substring(index + 1);
-    countWords(line);
-    index = remaining.indexOf('\n');
-  }
-});
-
-readStream.on('end', function() {
-  if (remaining.length > 0) {
-    countWords(remaining);
-  }
-
-  console.log(totWords);
-});
+module.exports = readDir;
